b5dfc32e6745448f5c83e0c7c8ac02fb463d4e35
Adityabajpai7 <meenalgbajpai@gmail.com>
2024-06-24 18:45:35 +0530

    end case improvision

--- even.py
+++ even.py
@@ -1,6 +1,6 @@
 user_input = int(input("enter the number: "))
 number = 0
-for number in range(user_input):
+for number in range(user_input + 1):
     if number % 2 == 0:
         print(number)
     else:
